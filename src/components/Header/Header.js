6cb7aa07263eb43fa57e3a44b1afbc77b5ab2f67
emonShimoul <emonshimoul@gmail.com>
2022-06-15 12:47:36 +0600

    eddited the navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,44 +1,37 @@
 import React from 'react';
 import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import logo from '../../images/logo.png';
 
 const Header = () => {
     return (
         <div>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                    <Navbar.Brand href="#" className='me-5'>
+                        <img style={{width: '100px'}} src={logo} />
+                        </Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
-                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action4">
-                            Another action
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action5">
-                            Something else here
-                        </NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#" disabled>
-                        Link
-                        </Nav.Link>
-                    </Nav>
-                    <Form className="d-flex">
-                        <Form.Control
-                        type="search"
-                        placeholder="Search"
-                        className="me-2"
-                        aria-label="Search"
-                        />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
+                        <Form className="d-flex me-auto">
+                            <Form.Control
+                            type="search"
+                            placeholder="Search Your Destination..."
+                            className="me-2 pe-5"
+                            aria-label="Search"
+                            />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                        <Nav
+                            className="my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="#action1">News</Nav.Link>
+                            <Nav.Link href="#action2">Destination</Nav.Link>
+                            <Nav.Link href="#action2">Blog</Nav.Link>
+                            <Nav.Link href="#action2">Contact</Nav.Link>
+                            <Button className='ms-3' variant="warning">Login</Button>
+                        </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
