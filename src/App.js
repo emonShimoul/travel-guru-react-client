db665ed86da01349314ce637824b5b98da4dc822
emonShimoul <emonshimoul@gmail.com>
2022-06-15 12:01:10 +0600

    set the routes using react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog/Blog';
+import Contact from './components/Contact/Contact';
+import Destination from './components/Destination/Destination';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/home' element={<Home />}></Route>
+          <Route path='/destination' element={<Destination />}></Route>
+          <Route path='/blog' element={<Blog />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
