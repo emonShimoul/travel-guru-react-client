2c5917a69978fb1cacd03831533bfbb4d6175171
emonShimoul <emonshimoul@gmail.com>
2022-06-16 22:38:20 +0600

    change in the slider

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,21 +23,21 @@ const Home = () => {
 
     return (
         <div className='container'>
-           <div className='row mt-5'>
+           <div className='row mt-5 pb-5'>
                 <div className='col-md-5 text-white'>
                     <h1>Cox's Bazar</h1>
                 </div>
                 <div id="carouselExampleControls" className="carousel slide col-md-7" data-bs-ride="carousel">
                     <div className="carousel-inner">
-                        <div className="carousel-item active">
-                        <img src={sajek} className="d-block w-100" alt="" />
-                        </div>
-                        <div className="carousel-item">
-                        <img src={sreemongol} className="d-block w-100" alt="" />
-                        </div>
-                        <div className="carousel-item">
-                        <img src={sundorbon} className="d-block w-100" alt="" />
-                        </div>
+                        <a href='#a' className="carousel-item active">
+                        <img src={sajek} className="d-block w-25 mx-auto" alt="" />
+                        </a>
+                        <a href='#b' className="carousel-item">
+                        <img src={sreemongol} className="d-block w-25 mx-auto" alt="" />
+                        </a>
+                        <a href='#c' className="carousel-item">
+                        <img src={sundorbon} className="d-block w-25 mx-auto" alt="" />
+                        </a>
                     </div>
                     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
