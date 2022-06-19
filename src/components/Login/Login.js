4f53f1217ab09ac514d9c4e1cef4f9243bbbf1e1
emonShimoul <emonshimoul@gmail.com>
2022-06-19 13:46:31 +0600

    change the login form style

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,22 +3,25 @@ import React from 'react';
 const Login = () => {
     return (
         <div style={{background: "white"}}>
-            <form className='mx-auto w-50 bg-white p-5'>
-                <div className="mb-5">
-                    {/* <label htmlFor="exampleInputEmail1" className="form-label">Email address</label> */}
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
-                </div>
-                <div className="mb-3">
-                    {/* <label htmlFor="exampleInputPassword1" className="form-label">Password</label> */}
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
-                </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+            <div className="container">
+                <form className='mx-auto w-50 bg-white p-5 border'>
+                <h3 className='pb-4 text-start'>Login</h3>
+                    <div className="mb-5">
+                        {/* <label htmlFor="exampleInputEmail1" className="form-label">Email address</label> */}
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        {/* <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div> */}
+                    </div>
+                    <div className="mb-3">
+                        {/* <label htmlFor="exampleInputPassword1" className="form-label">Password</label> */}
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
     );
 };
