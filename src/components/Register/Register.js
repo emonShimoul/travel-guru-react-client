0731c81265161130781b8c4df58fe13dbf04d230
emonShimoul <emonshimoul@gmail.com>
2022-06-22 11:02:20 +0600

    added media query for register button

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -7,13 +7,22 @@ const Register = () => {
             <div className="container pt-3">
                 <form className='mx-auto w-50 bg-white px-5 pt-5 pb-3 border'>
                 <h3 className='pb-4 text-start'>Create an Account</h3>
-                    <div className="mb-3">
+                    <div className="mb-4">
                         <input type="text" className="form-control" id="exampleInputEmail1" placeholder='First Name' />
                     </div>
-                    <div className="mb-3">
-                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Last Name' />
+                    <div className="mb-4">
+                        <input type="text" className="form-control" id="exampleInputPassword1" placeholder='Last Name' />
                     </div>
-                    <button type="submit" className="btn btn-warning mb-3">Create an Account</button>
+                    <div className="mb-4">
+                        <input type="email" className="form-control" id="exampleInputEmail1" placeholder='Email' />
+                    </div>
+                    <div className="mb-4">
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Password' />
+                    </div>
+                    <div className="mb-4">
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Confirm Password' />
+                    </div>
+                    <button type="submit" className="btn register-btn btn-warning mb-3">Create an Account</button>
                     <p>Already have an account? <Link to="/login" className='text-warning'>Login</Link></p>
                 </form><br />
                 <p>------------------------------------------ Or ------------------------------------------</p>
